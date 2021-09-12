213f3912ad570c870887883438ad53c58ff5d895
Paul <git@dev-paul.de>
2021-09-12 21:38:36 +0200

    Made a small navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-navbar type="dark" variant="primary">
+    <b-navbar-brand class="pl-2">Snipe-IT Kiosk</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
