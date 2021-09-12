96c3cf3ac021116e053d12c2bddcb32358996371
Paul <git@dev-paul.de>
2021-09-12 21:36:50 +0200

    Installed some more dependencies and included them into the project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
