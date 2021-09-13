3090eecb3439865000085ada422c5ec8d9d94a28
Paul <git@dev-paul.de>
2021-09-13 16:17:56 +0200

    Return from scan results

--- src/views/ScanResult.vue
+++ src/views/ScanResult.vue
@@ -1,17 +1,24 @@
 <template>
   <b-container class="text-center mt-5">
     <AssetDisplay :asset="this.$route.params.result" />
+    <KeyboardReader :match="/^Backspace/" @read="$router.back()" />
   </b-container>
 </template>
 
 <script>
 import AssetDisplay from "../components/AssetDisplay.vue";
+import KeyboardReader from "../components/KeyboardReader.vue";
 export default {
-  components: { AssetDisplay },
+  components: { AssetDisplay, KeyboardReader },
   name: "ScanResult",
   data: () => ({
     showAlert: false,
     loading: false,
   }),
+  beforeMount: function () {
+    if (this.$route.params.result == null) {
+      window.location = "#/scan";
+    }
+  },
 };
 </script>
