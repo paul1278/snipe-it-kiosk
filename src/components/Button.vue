a1aecdb1ffcc0f62c4dc510e7a6f3f69c4c5a413
Paul <git@dev-paul.de>
2021-09-23 12:00:19 +0200

    New button which allows to directly register a shortcut

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-btn
+    v-bind="$attrs"
+    @click="
+      (e) => (this.href ? this.$router.push(this.href) : this.$emit('click', e))
+    "
+  >
+    <slot />
+    <Shortcut
+      v-if="this.shortcut != null"
+      :shortcut="this.shortcut"
+      :href="this.href"
+      @click="(e) => this.$emit('click', e)"
+    />
+  </b-btn>
+</template>
+
+<script>
+import Shortcut from "./Shortcut.vue";
+export default {
+  name: "Button",
+  components: { Shortcut },
+  props: {
+    href: {
+      type: String,
+      default: null,
+    },
+    shortcut: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style></style>
