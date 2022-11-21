66a4c65712a7cbbe25c588e4973faef049d3a425
Paul <git@dev-paul.de>
2022-11-21 15:19:56 +0100

    Doing some things to a really small, no-dependency backend

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+
+const upstreamBase = "";
+
+const server = http.createServer((req, res) => {
+  let upstream = http.request(
+    {
+      host: upstreamBase,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+      data: req.data,
+    },
+    (resp) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Authorization,Content-Type"
+      );
+      resp.pipe(res);
+    }
+  );
+
+  req.pipe(upstream);
+});
+
+server.listen(8081, () => {
+  console.log(`Server is running on http://127.0.0.1:8081`);
+});
